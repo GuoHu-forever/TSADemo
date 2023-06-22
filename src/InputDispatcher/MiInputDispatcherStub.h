b11c6107624d4f253420a9c53b61e34b59662e13
guohu <guohu@xiaomi.com>
2023-06-23 01:31:03 +0800

    动态库跨平台

--- src/InputDispatcher/MiInputDispatcherStub.h
+++ src/InputDispatcher/MiInputDispatcherStub.h
@@ -2,10 +2,23 @@
 #define MIINPUTDISPATCHERSTUB_H
 #include"IMiInputdispatcherstub.h"
 #include<mutex>
+#ifdef _WIN32
+    #include <windows.h>
+    #define LIB_HANDLE HMODULE
+    #define LIB_LOAD(libname) LoadLibrary(libname)
+    #define LIB_SYMBOL(libhandle, symbolname) GetProcAddress(libhandle, symbolname)
+    #define LIB_CLOSE(libhandle) FreeLibrary(libhandle)
+#else
+    #include <dlfcn.h>
+    #define LIB_HANDLE void*
+    #define LIB_LOAD(libname) dlopen(libname, RTLD_LAZY)
+    #define LIB_SYMBOL(libhandle, symbolname) dlsym(libhandle, symbolname)
+    #define LIB_CLOSE(libhandle) dlclose(libhandle)
+#endif
 class MiInputDispatcherStub
 {
 private:
-    static void* sLibMiInputDispatcherImpl;
+    static LIB_HANDLE sLibMiInputDispatcherImpl;
     static IMiInputDispatcherStub* sStubImplInstance;
     static std::mutex sLock;
     static bool inited;
